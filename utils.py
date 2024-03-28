8964f10ecf6c4717abcd5d62be9b0e6e24490e1b
Steven Gutstein <s.m.gutstein@gmail.com>
2024-03-27 22:37:21 -0400

    Added profiling & timim=ng decorators

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import configparser
+import contextlib
+import cProfile
 import subprocess as sp
+import time
+
 
 def read_config_file(filename):
     config = configparser.ConfigParser()
@@ -25,3 +29,30 @@ def get_git_hash():
     diff_str = sp.check_output(['git', 'diff']).decode("utf-8").strip()
     output_str = hash_str + '\n\n' + diff_str
     return output_str
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+
+def profile_decorator(func):
+    def wrapper(*args, **kwargs):
+        profiler = cProfile.Profile()
+        
+        profiler.enable()
+        result = func(*args, **kwargs)
+        profiler.disable()
+
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        with open(f'profiler_output_{timestamp}.txt', 'w') as f:
+            with contextlib.redirect_stdout(f):
+                profiler.print_stats(sort='cumtime')
+
+        return result
+
+    return wrapper
